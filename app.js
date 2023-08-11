ef658abb98ef9919e9842d3654b65ec6102cc097
Pedro Henrique <henriquepedro1912@gmail.com>
2023-08-11 13:27:18 -0300

    correção no favicon

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
